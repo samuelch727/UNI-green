3875f8df18ba9ebb485cc895d26815934318c97d
LeeKaSin0518 <ivylee950@gmail.com>
2021-06-18 13:06:45 +0800

    Added request.ts

---
+++ backend/src/models/Request.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema({
+  orderid: { type: Number, required: true },
+  productid: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  status: { type: Boolean, required: true },
+  paymentStatus: { type: Boolean, required: true },
+  price: { type: mongoose.Types.Decimal128, required: true },
+});
+
+module.exports = mongoose.model("User", requestSchema);
