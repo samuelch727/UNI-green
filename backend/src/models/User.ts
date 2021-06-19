d46b008914da819d225519fac68e11e731dbcd56
samuelch727 <samuelchan727@gmail.com>
2021-06-19 17:53:40 +0800

    Updated user collection data validation

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  tel: { type: String, required: true },
-  email: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true,
+    },
+    password: { type: String, required: true },
+    tel: { type: String, required: true },
+    email: {
+      type: String,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true,
+    },
+    subUserId: [{ type: mongoose.Types.ObjectId, ref: "SubUser" }],
+  },
+  { timestamps: true }
+);
 
 mongoose.model("User", UserSchema);
