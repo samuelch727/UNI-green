2abd70fdd093cd56d049cd25fa3af4e7a8806ce2
LeeKaSin0518 <ivylee950@gmail.com>
2021-08-09 00:46:35 +0800

    Upload Financial report.tsx & Product[not yet finish]

---
+++ frontend/demo/admin-panel/src/pages/Product.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import { eventNames } from "process";
+
+function Product(props: any){
+    function handleSubmit() {
+        
+    }
+
+
+
+    
+}
